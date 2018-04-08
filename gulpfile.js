1a901bc6732ce1193855ea29e6692f787c83690b
Николай Регулярный <regularniy@gmail.com>
2018-04-08 20:49:06 +1000

    gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,24 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
 
-gulp.task('sass', function () {
-    gulp.src('scss/styles.scss')
-        .pipe(sass({includePaths: ['scss']}))
-        .pipe(gulp.dest('css'));
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+  browserSync.init({
+    server: "./"
+  });
+
+  gulp.watch("scss/*.scss", ['sass']);
+  gulp.watch("*.html").on('change', browserSync.reload);
 });
 
-gulp.task('browser-sync', function() {
-    browserSync.init(["css/*.css", "js/*.js"], {
-        server: {
-            baseDir: "./"
-        }
-    });
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+  return gulp.src("scss/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("css"))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('default', ['sass', 'browser-sync'], function () {
-    gulp.watch("scss/*.scss", ['sass']);
-});
+gulp.task('default', ['serve']);
